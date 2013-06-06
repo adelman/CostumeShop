0d0e8bf451ae14ce5142d7e8c75d33c9b7179199
Matt Adelman <madelman@wesleyan.edu>
2013-06-06 10:14:57 -0400

    Initial commit of new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CostumeShop::Application.config.secret_token = '6379cbf8ac4ae949e4f15f63ed347f646bfeb1a7c15e79359a24afe72c67869e0933f693c55cb9d99c1fad0affd8ee23fe23d53cfd48258cb2dfab0e605ea71e'
