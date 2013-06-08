4d1e4817748a8c7e4d976db035a56a2e42d4a195
Matt Adelman <madelman@wesleyan.edu>
2013-06-07 22:30:17 -0400

    Timezone take 3

--- db/migrate/20130606145709_change_dates.rb
+++ db/migrate/20130606145709_change_dates.rb
@@ -1,7 +1,7 @@
 class ChangeDates < ActiveRecord::Migration
   def change
-    change_column :agreements, :perfstart, :datetime, :offset => "-0500"
-    change_column :agreements, :perfend, :datetime, :offset => "-0500"
-    change_column :agreements, :duedate, :datetime, :offset => "-0500"
+    change_column :agreements, :perfstart, :timestamp
+    change_column :agreements, :perfend, :timestamp
+    change_column :agreements, :duedate, :timestamp
   end
 end
