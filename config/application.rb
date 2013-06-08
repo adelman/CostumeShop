dd32d17f65f4566c29667908fdf49afbcc1cb5e0
Matt Adelman <madelman@wesleyan.edu>
2013-06-07 23:26:40 -0400

    Timezone take 4

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module CostumeShop
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.time_zone = 'Eastern Time (US & Canada)'
+config.time_zone = 'Central Time (US & Canada)'
 
   end
 end
