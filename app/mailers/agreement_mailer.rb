c1a92cd86cef11eba2a1ade79cbd0a49a812e162
Matt Adelman <madelman@wesleyan.edu>
2013-06-11 13:52:18 -0400

    Adding attachments to emails v1

--- app/mailers/agreement_mailer.rb
+++ app/mailers/agreement_mailer.rb
@@ -3,8 +3,15 @@ class AgreementMailer < ActionMailer::Base
 
   def welcome_email(agg)
     @agreement = agg
+    attachments['BorrowingTerms.pdf'] = File.read('../../public/BorrowingTerms.pdf')
     email_with_name = "#{agg.name} <#{agg.email}>"
-    mail(:to => email_with_name, :subject => "Here is a copy of your lending agreement.")
+    mail(:to => email_with_name, :subject => "Costume Shop Lending Confirmation - Costumes Due #{agg.duedate}")
   end
 
+  def reminder_email(agg)
+    @agreement = agg
+    email_with_name = "#{agg.name} <#{agg.email}>"
+    mail(:to => email_with_name, :subject => "REMINDER: Costumes Due #{agg.duedate}"
+  end
+  
 end
