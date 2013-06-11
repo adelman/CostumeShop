3b1ff7c1fe6915f4dcae734153c42c560ae4f2bc
Matt Adelman <madelman@wesleyan.edu>
2013-06-11 19:34:32 -0400

    Still trying email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
 
   def authenticate
     authenticate_or_request_with_http_basic do |user, pass|
-      user==APP_CONFIG['username'] && pass=APP_CONFIG['password']
+      user==USERNAME && pass=PASSWORD
     end
   end
 end
