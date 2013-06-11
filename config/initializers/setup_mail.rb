134ab55c9b8bda76071279de30cc22744e8dcc9d
Matt Adelman <madelman@wesleyan.edu>
2013-06-11 19:34:32 -0400

    Still trying email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "wesleyan.edu",
   :user_name            => "costumeshop@wesleyan.edu",
-  :password             => APP_CONFIG['email_pass'],
+  :password             => EMAIL_PASS,
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
