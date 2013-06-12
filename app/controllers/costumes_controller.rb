1e2c4adaca12af28606c7fe9dc99cadeb9dfb67e
Matt Adelman <madelman@wesleyan.edu>
2013-06-12 17:25:05 -0400

    Not using costume controller anymore...

--- app/controllers/costumes_controller.rb
+++
@@ -1,7 +0,0 @@
-class CostumesController < ApplicationController
-  def create
-    @agreement = Agreement.find(params[:agreement_id])
-    @costume = @agreement.costumes.create(params[:costume])
-    redirect_to agreement_path(@agreement)
-  end
-end
