9e08b19f53870de7d36696ea1a0f2258295808b1
Matt Adelman <madelman@wesleyan.edu>
2013-06-12 17:25:54 -0400

    Working email from different account

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "wesleyan.edu",
-  :user_name            => "costumeshop@wesleyan.edu",
-  :password             => ENV['EMAIL_PASS'],
+  :user_name            => "madelman@wesleyan.edu",
+  :password             => ENV['EMAIL_PASS2'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
