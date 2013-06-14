ec2d441653499b7e28fbb6ffbc5710b5ccc8e5e4
Matt Adelman <madelman@wesleyan.edu>
2013-06-14 12:18:37 -0400

    In theory, costume shop address works

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "wesleyan.edu",
-  :user_name            => "madelman@wesleyan.edu",
-  :password             => ENV['EMAIL_PASS2'],
+  :user_name            => "costumeshop@wesleyan.edu",
+  :password             => ENV['EMAIL_PASS'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
