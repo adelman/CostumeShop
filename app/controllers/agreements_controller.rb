fc539503c6b57a5b0f5421b6cb59a0622f36722f
Matt Adelman <madelman@wesleyan.edu>
2014-02-15 14:09:11 -0500

    Fixing error v2

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -102,7 +102,9 @@ class AgreementsController < ApplicationController
     @agreement.photo49 = upload_photo(@agreement.photo49)
 
     respond_to do |format|
-      if @agreement.save
+      logger.info("Inside respond")
+      if @agreement.save!
+        logger.info("Inside save")
         AgreementMailer.welcome_email(@agreement).deliver
         format.html { redirect_to @agreement, :notice => 'Agreement was successfully created.' }
         format.json { render :json => @agreement, :status => :created, :location => @agreement }
